cb1e7977228ab842ac61e10e4ec0868aaa3ebf5b
taeju_jung(ROG) <taejustar@gmail.com>
2020-11-30 23:28:31 +0900

    lags

--- NGP_TermProject/NGP_TermProject/NGP_TermProject.cpp
+++ NGP_TermProject/NGP_TermProject/NGP_TermProject.cpp
@@ -89,7 +89,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // 기본 메시지 루프입니다:
     while (1)
     {
-        Sleep(10);
+        //Sleep(10);
         if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
         {
             if (msg.message == WM_QUIT)
